25d7f2b59b640a8ad04ead1fd9f3a531d81d2cd2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-02 19:47:44 +0200

    add 1 test for `bin/rename.py`

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -20,22 +20,8 @@ def test_failure_scenario_within_construct_new_filename():
 
 
 def test_failure_scenario_due_to_non_existent_directory():
-    #     # Create a temporary directory, one for each test run.
-    #     # Create a file in a test
-
-    #     # Arrange.
-    #     filename = dt.datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S.%f")
-
-    #     for f in os.listdir("data"):
-    #         shutil.copy(
-    #             os.path.join("data", f),
-    #             tmp_path / f,
-    #         )
-
-    #     print(tmp_path)
-
     # Arrange.
-    cmd = [
+    command = [
         "python",
         "bin/rename.py",
         "--directory",
@@ -43,27 +29,54 @@ def test_failure_scenario_due_to_non_existent_directory():
     ]
 
     # Act.
-    # fmt: off
-    c_p: subprocess.CompletedProcess = subprocess.run(
-        cmd
-    )
-    # fmt: on
+    c_p: subprocess.CompletedProcess = subprocess.run(command)
 
-    #     # Assert.
-
+    # Assert.
     assert c_p.returncode == 1
 
 
 def test_failure_scenario_due_to_not_a_directory():
     # Arrange.
-    cmd = [
+    command = [
         "python",
         "bin/rename.py",
         "--directory=README.md",
     ]
 
     # Act.
-    c_p: subprocess.CompletedProcess = subprocess.run(cmd)
+    c_p: subprocess.CompletedProcess = subprocess.run(command)
 
     # Assert.
     assert c_p.returncode == 2
+
+
+def test_success_scenario(tmp_path):
+    """
+    Create a temporary directory, one for each run of this test case.
+    """
+
+    # Arrange.
+    for f in os.listdir("data"):
+        shutil.copy(
+            os.path.join("data", f),
+            tmp_path / f,
+        )
+
+    command = [
+        "python",
+        "bin/rename.py",
+        "--directory",
+        tmp_path,
+    ]
+
+    # Act.
+    c_p: subprocess.CompletedProcess = subprocess.run(command)
+
+    # Assert.
+    assert c_p.returncode == 0
+
+    assert set(os.listdir(tmp_path)) == {
+        "2022-02-09-20-43-30.txt",
+        "2022-02-09-20-43-33.txt",
+        "2022-02-09-20-43-38.txt",
+    }
