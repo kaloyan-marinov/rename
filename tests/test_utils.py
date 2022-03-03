421c7711688792143d95434a5dde3270fcf18fd9
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-03 18:24:46 +0200

    demonstrate a bug by writing a test case, which currently fails

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,16 @@
 from src.utils import construct_new_filename
 
 
-def test_success_scenario_within_construct_new_filename():
+def test_success_scenario_within_construct_new_filename_1():
     original_filename = "Screen Shot 2022-02-09 at 20.43.30.png"
     new_filename = construct_new_filename(original_filename)
     assert new_filename == "2022-02-09-20-43-30.png"
+
+
+def test_success_scenarion_within_construct_new_filename_2():
+    original_filename = "Screen Shot 2022-03-03 at 9.21.21.png"
+    new_filename = construct_new_filename(original_filename)
+    assert new_filename == "2022-03-03-09-21-21.png"
 
 
 def test_failure_scenario_within_construct_new_filename():
