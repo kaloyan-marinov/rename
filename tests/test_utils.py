aede0a31da8465b63ffab42d0ec2393a318cd63b
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-12-08 06:58:41 +0100

    3: restore the tests in `tests/test_utils.py` to a PASSing state

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,41 @@
+import pytest
+
 from src.utils import construct_new_filename
 
 
-def test_success_scenario_within_construct_new_filename_1():
+@pytest.fixture
+def reg_expr_for_macos_screenshot():
+    return r"(\d+-\d+-\d+) at (\d+).(\d+).(\d+).(\w+)"
+
+
+def test_success_scenario_within_construct_new_filename_1(
+    reg_expr_for_macos_screenshot,
+):
     original_filename = "Screen Shot 2022-02-09 at 20.43.30.png"
-    new_filename = construct_new_filename(original_filename)
+    new_filename = construct_new_filename(
+        original_filename,
+        reg_expr_for_macos_screenshot,
+    )
     assert new_filename == "2022-02-09-20-43-30.png"
 
 
-def test_success_scenarion_within_construct_new_filename_2():
+def test_success_scenarion_within_construct_new_filename_2(
+    reg_expr_for_macos_screenshot,
+):
     original_filename = "Screen Shot 2022-03-03 at 9.21.21.png"
-    new_filename = construct_new_filename(original_filename)
+    new_filename = construct_new_filename(
+        original_filename,
+        reg_expr_for_macos_screenshot,
+    )
     assert new_filename == "2022-03-03-09-21-21.png"
 
 
-def test_failure_scenario_within_construct_new_filename():
+def test_failure_scenario_within_construct_new_filename(
+    reg_expr_for_macos_screenshot,
+):
     original_filename = "does-not-match-the-function-s-regular-expression-pattern.txt"
-    new_filename = construct_new_filename(original_filename)
+    new_filename = construct_new_filename(
+        original_filename,
+        reg_expr_for_macos_screenshot,
+    )
     assert new_filename is None
